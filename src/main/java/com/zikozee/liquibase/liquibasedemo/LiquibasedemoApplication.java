bdaa8f29b74304919b7c1bacdbd845da1e758423
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2020-07-27 09:23:07 +0100

    Initial commit

---
+++ src/main/java/com/zikozee/liquibase/liquibasedemo/LiquibasedemoApplication.java
@@ -0,0 +1,13 @@
+package com.zikozee.liquibase.liquibasedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibasedemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibasedemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zikozee/liquibase/liquibasedemo/LiquibasedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zikozee.liquibase.liquibasedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibasedemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
